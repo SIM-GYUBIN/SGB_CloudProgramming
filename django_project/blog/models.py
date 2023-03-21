0d6b9b96ac1ebc687dd7290353a61e1514c903e4
Sim-GyuBin <skb0606@gmail.com>
2023-03-21 15:21:44 +0900

    Post에 updated_at 추가

--- django_project/blog/models.py
+++ django_project/blog/models.py
@@ -4,7 +4,8 @@ class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
 
-    created_at = models.DateTimeField()
-
+    #새로운거 추가됐을때 넣어라, 시간 오토로 저장
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     def __str__(self):
         return f'[{self.pk}]{self.title}'
