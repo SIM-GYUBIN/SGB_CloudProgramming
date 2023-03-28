3994f0bbb45eb1a273960bc5ae453bc2f521f0d4
Sim-GyuBin <skb0606@gmail.com>
2023-03-28 15:39:40 +0900

    CBV refactoring

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<int:post_num>/', views.single_post_page),
-    path('', views.index),
+    path('', views.PostList.as_view()),
+    path('<int:pk>/', views.PostDetail.as_view()),
+
 
 ]
--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,18 +1,19 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView, DetailView
 from .models import Post
 # 풀 경로로 import하면 문제 생김
 
 def index(request) :
     # 사용자의 요청을 받아 이런저런일을 여기서
     posts = Post.objects.all().order_by('-pk')
-    return render(request, 'blog/index.html',{
+    return render(request, 'blog/post_list.html', {
         'posts': posts,
     })
 
-def single_post_page(request, post_num) :
-    post = Post.objects.get(pk=post_num)
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'
 
-    return render(request, 'blog/single_post_page.html', {
-        'post': post,
-    })
+
+class PostDetail(DetailView):
+    model = Post
